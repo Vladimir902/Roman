ff5785dbeec66a582667adf06c9c2c12eacb9e94
Vladimir902 <vladimir.malakovski@kinandcarta.com>
2024-08-12 10:41:29 +0200

    Initial commit of roman to integer program

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
+public class Main {
+        public static void main(String[] args) {
+                try {
+                        Scanner scanner = new Scanner(System.in);  //Generating user input
+                        System.out.println("Enter a roman number: ");
+                        String input = scanner.nextLine();  //user input of a roman number
+
+                        if (input.isEmpty() || input == null) {  //Error handling for empty input
+                                throw new IllegalArgumentException("Input cannot be empty");
+                        }
+
+                        int result = Solution.romanToInt(input);   //Error handling for roman input (The input should be String)
+                        System.out.println("Integer value: " + result);
+                } catch (IllegalArgumentException e) {
+                        System.out.println("Invalid input: " + e.getMessage());
+                } catch (Exception e) {
+                        System.out.println("An error occurred: " + e.getMessage());
+
+
+                }
+        }
+}
---
+++ src/main/java/org/example/Solution.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+        public static int romanToInt(String str) {  //static method so we can call it without instance
+
+            if (str == null || str.length() == 0)  //checking if the input is null or 0
+                return 0;
+
+            Map<Character, Integer> map = new HashMap<Character, Integer>();  //adding elements to the map
+            map.put('I', 1);
+            map.put('V', 5);
+            map.put('X', 10);
+            map.put('L', 50);
+            map.put('C', 100);
+            map.put('D', 500);
+            map.put('M', 1000);
+
+            int len = str.length();
+            int result = map.get(str.charAt(len - 1));  //initializing the result with the last character
+
+            for (int i = len - 2; i >= 0; i--) {   //processing the characters from right to left
+                if (map.get(str.charAt(i + 1)) <= map.get(str.charAt(i)))
+                    result = result + map.get(str.charAt(i));
+                else
+                    result = result - map.get(str.charAt(i));
+            }
+            return result;
+        }
+    }
